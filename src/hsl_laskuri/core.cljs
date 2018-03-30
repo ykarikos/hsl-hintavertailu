a42ea83b9dc940814f32a86fca707b2ccaca7747
Yrjö Kari-Koskinen <yrjo.kari-koskinen@futurice.com>
2018-03-31 01:44:39 +0300

    kausilippulaskuri

--- src/hsl_laskuri/core.cljs
+++ src/hsl_laskuri/core.cljs
@@ -5,8 +5,44 @@
 ;; -------------------------
 ;; Views
 
+(def fortnight-price
+  28.7)
+
+(def extra-day-price
+  1.63)
+
+(def fortnight
+  14)
+
+(def price-data
+  (r/atom {:days fortnight :price fortnight-price}))
+
+(defn round [n]
+  (/ (Math/round (* n 100)) 100))
+
+(defn calc-price [days]
+  (if (<= days fortnight)
+    fortnight-price
+    (round (+ fortnight-price (* (- days fortnight) extra-day-price)))))
+
+(defn calc-price-data []
+  (let [{:keys [days] :as data} @price-data]
+    (assoc data :price (calc-price days))))
+
+(defn slider [param value min max]
+  [:input {:type "range" :value value :min min :max max
+           :style {:width "100%"}
+           :on-change (fn [e]
+                        (swap! price-data assoc param (.. e -target -value)))}])
+
 (defn home-page []
-  [:div [:h2 "Welcome to Reagent"]])
+  (let [{:keys [days price]} (calc-price-data)]
+    [:div
+     [:h1 "HSL-hintalaskuri"]
+     [:div "Voimassaoloaika " (int days) " päivää"
+       [slider :days days 14 366]]
+     [:div "Hinta: " price " €"]]))
+
 
 ;; -------------------------
 ;; Initialize app

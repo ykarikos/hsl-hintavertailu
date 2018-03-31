a7d5fee2391e02e1384f9a4db97bba7a78a6dc80
Yrjö Kari-Koskinen <yrjo.kari-koskinen@futurice.com>
2018-03-31 03:06:03 +0300

    add regional ticket prices

--- src/hsl_laskuri/core.cljs
+++ src/hsl_laskuri/core.cljs
@@ -9,7 +9,8 @@
 
 (def price-data
   (r/atom {:travel-days 10
-           :days 14}))
+           :days 14
+           :ticket-type :one-zone}))
 
 (defn slider [param value min max]
   [:input {:type "range" :value value :min min :max max
@@ -17,16 +18,30 @@
            :on-change (fn [e]
                         (swap! price-data assoc param (.. e -target -value)))}])
 
+(defn radio [label name value selected-value]
+  (let [checked (= value selected-value)]
+    [:label label
+     [:input {:type "radio"
+              :name name
+              :checked checked
+              :value value
+              :on-click
+                (fn [e]
+                  (when (.. e -target -value) (swap! price-data assoc :ticket-type value)))}]]))
+
 (defn format-price [n]
   (clojure.string/replace
     (goog.string.format "%.2f" n)
     "." ","))
 
 (defn home-page []
-  (let [{:keys [travel-days days season-price single-price]}
+  (let [{:keys [travel-days days season-price single-price ticket-type]}
         (c/calc-price-data @price-data)]
     [:div
-     [:h1 "HSL:n kausi- ja kertalipun hintavertailu"]
+     [:div "Lipun tyyppi: "
+      [radio "Yksi vyöhyke" "type" :one-zone ticket-type]
+      " "
+      [radio "Seutu" "type" :regional ticket-type]]
      [:div "Voimassaoloaika " (int days) " päivää"
       [slider :days days 14 366]]
      [:div "Edestakaisia matkoja " (int travel-days) " päivänä"
@@ -34,9 +49,9 @@
      [:div "Kertalippujen hinta: " (format-price season-price) " €"]
      [:div "Yksittäislippujen hinta: " (format-price single-price) " €"]
      [:h2 (if (> single-price season-price)
-             "Kausilippu"
-             "Kertalippu")
-          " on halvempi!"]]))
+            "Kausilippu"
+            "Kertalippu")
+      " on halvempi!"]]))
 
 
 ;; -------------------------
--- src/hsl_laskuri/price_calculator.cljs
+++ src/hsl_laskuri/price_calculator.cljs
@@ -1,13 +1,16 @@
 (ns hsl-laskuri.price-calculator)
 
 (def fortnight-price
-  28.7)
+  {:one-zone 28.7
+   :regional 55.7})
 
 (def extra-day-price
-  1.63)
+  {:one-zone 1.63
+   :regional 3.18})
 
 (def single-price
-  2.2)
+  {:one-zone 2.2
+   :regional 4.2})
 
 (def fortnight
   14)
@@ -15,16 +18,18 @@
 (defn- round [n]
   (/ (Math/round (* n 100)) 100))
 
-(defn- calc-season-price [days]
-  (if (<= days fortnight)
-    fortnight-price
-    (round (+ fortnight-price (* (- days fortnight) extra-day-price)))))
+(defn- calc-season-price [days ticket-type]
+  (let [f-price (ticket-type fortnight-price)
+        e-price (ticket-type extra-day-price)]
+    (if (<= days fortnight)
+      f-price
+      (round (+ f-price (* (- days fortnight) e-price))))))
 
-(defn- calc-single-price [days]
-  (round (* 2 days single-price)))
+(defn- calc-single-price [days ticket-type]
+  (round (* 2 days (ticket-type single-price))))
 
 (defn calc-price-data [price-data]
-  (let [{:keys [days travel-days] :as data} price-data]
+  (let [{:keys [days travel-days ticket-type] :as data} price-data]
     (assoc data
-           :season-price (calc-season-price days)
-           :single-price (calc-single-price travel-days))))
+           :season-price (calc-season-price days ticket-type)
+           :single-price (calc-single-price travel-days ticket-type))))
